29b4c46fff53e0aa6129c0d2e65e3a213743ee8c
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-12-05 20:10:53 -0800

    created burger model

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,21 @@ const orm = require("../config/orm.js");
 
 // code to call orm functions using burger specific input for the orm
 let burger = {
-
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
 };
 
 // Export the database functions for the controller
