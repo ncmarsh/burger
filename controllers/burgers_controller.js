b1a825600f2c3850b24524c6aeed42f5c53a5122
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-12-07 19:50:31 -0800

    updating package.json

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,14 +9,20 @@ const burger = require("../models/burger.js");
 // Routes
 // Show all the burgers
 router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+    burger.selectAll(function(err, data) {
+        if (err) {
+            return res.status(500).end();
+        };
         res.render("index", { burger: data });
     });
 });
 
 // Add new burger from user input
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, 0], function(result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, 0], function(err, result) {
+        if (err) {
+            return res.status(500).end();
+        };
         res.json({id: result.insertId});
     });
 });
@@ -30,11 +36,14 @@ router.put("/api/burgers/:id", function(req, res) {
             devoured: req.body.devoured
         },
         eaten,
-        function(result) {
+        function(err, result) {
+            if (err) {
+                return res.status(500).end();
+            };
             if (result.changedRows === 0) {
                 // if no rows were changed, Id does not exist, is 404
                 return res.status(404).end();
-            }
+            };
             res.status(200).end();
         }
     );
