e83f6de1499174fe50f81d6118ad5bccdb8287de
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-12-07 20:11:13 -0800

    updating port

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Establish connection to MySQL server
 const connection = mysql.createConnection({
     host: "localhost",
-    port: process.env.PORT || process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DB,
