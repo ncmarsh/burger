37d6d05f6e8a0e7d07d4501be2fef1f14d3c1ff3
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-12-08 18:24:59 -0800

    updating connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,17 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 // Establish connection to MySQL server
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DB,
+    });
+};
 
 // Start connection to MySQL
 connection.connect(function(err) {
